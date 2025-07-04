7a1195fdf8044279e34614c6ee04d5206449158a
Álan Christian <xalanchristian@outlook.com>
2025-07-04 15:50:44 -0300

    Criado o componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+
+import { Logo } from './components/Logo/Index';
+import { Heading } from './components/Heading/Index';
+import { Container } from './components/Container/Index';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
       </Container>
     </>
   );
