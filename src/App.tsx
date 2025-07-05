83461956366359935f61bdae0b225d3d8f67b81a
Álan Christian <xalanchristian@outlook.com>
2025-07-05 10:19:01 -0300

    Criado o componente DefaultInput e styles.css

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo/Index';
 import { Container } from './components/Container/Index';
 import { Menu } from './components/Menu/Index';
 import { CountDown } from './components/CountDown/Index';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -17,6 +18,28 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuinput'
+              type='text'
+              labelText='id'
+              placeholder='Digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
