e09deecf6dbb71445851c41c9daab2cff838651d
Álan Christian <xalanchristian@outlook.com>
2025-07-07 12:46:43 -0300

    Validando o formulário

--- src/components/Mainform/index.tsx
+++ src/components/Mainform/index.tsx
@@ -3,12 +3,47 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInpult = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (taskNameInpult.current === null) return;
+
+    const taskName = taskNameInpult.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        acitivetask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formatedSecondsRemaining: '00:00',
+        task: [...prevState.task, newTask],
+        config: { ...prevState.config },
+      };
+    });
   }
   return (
     <form onSubmit={handleCreateNewTask} action='' className='form'>
